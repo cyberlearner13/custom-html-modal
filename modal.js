99455a0961dd2e4983ae5699dfca8cb2aaccd557
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2022-06-06 20:49:00 +0530

    Putting checks and removing complexity from main code

--- modal.js
+++ modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
     constructor(){
         super();
         this.attachShadow({ mode: 'open' });
+        this.isOpen = false;
         this.shadowRoot.innerHTML = `
             <style>
                 #backdrop {
@@ -77,6 +78,21 @@ class Modal extends HTMLElement {
         `;
     }
 
+    attributeChangedCallback(name, oldValue, newValue){
+        if(this.hasAttribute('opened')){
+            this.isOpen = true;
+        } else {
+            this.isOpen = false;
+        }
+    }
+
+    static get observedAttributes() {
+        return ['opened'];
+    }
+
+    open() {
+        this.setAttribute('opened', '');
+    }
 }
 
 customElements.define('vax-modal', Modal);
